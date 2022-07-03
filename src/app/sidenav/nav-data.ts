620d670d6e7bb9d7d18956f0114fb3ed43e88cc0
Ugochukwu Umerie <umerieugochukwu@gmail.com>
2022-07-03 16:28:17 +0100

    Dashboard Layout
    Created the dashboard layout and added animations to the overall sidebar transition and close button

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,37 @@
+export const navbarData = [
+    { 
+        routelink: 'dashboard',
+        icon: 'fa fa-home',
+        label: 'Dashboard'
+    },
+    { 
+        routelink: 'products',
+        icon: 'fa fa-box-open',
+        label: 'Products'
+    },
+    { 
+        routelink: 'statistics',
+        icon: 'fa fa-chart-bar',
+        label: 'Statistics'
+    },
+    { 
+        routelink: 'coupons',
+        icon: 'fa fa-tags',
+        label: 'Coupons'
+    },
+    { 
+        routelink: 'pages',
+        icon: 'fa fa-file',
+        label: 'Pages'
+    },
+    { 
+        routelink: 'media',
+        icon: 'fa fa-camera',
+        label: 'Media'
+    },
+    { 
+        routelink: 'settings',
+        icon: 'fa fa-cog',
+        label: 'Settings'
+    },
+]
